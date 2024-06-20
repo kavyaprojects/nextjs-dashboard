40f2a9f474c8358c1e40688aef263952ccefa948
kavyaprojects <kavya.projectsdigitas@gmail.com>
2024-06-20 13:37:26 +0530

    with storybook

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,15 @@
+
+import React from 'react';
+import HeroBanner from '../../stories/HeroBanner';
+
 export default function Page() {
-    return <p>Dashboard Page</p>;
+    return (
+    <div className='flex flex-col'>
+    <HeroBanner
+      title="workspace"
+      subtitle="around the"
+      description='This text is from storybook herobanner'
+    />
+    <div>this is sample one</div>
+  </div>)
   }
